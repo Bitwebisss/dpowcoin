e0c4e104e11848a3c7578080b80fbc3f14436a01
Bitwebisss <196428942+Bitwebisss@users.noreply.github.com>
2026-04-28 23:10:20 +0300

    Update miner_tests.cpp

--- src/test/miner_tests.cpp
+++ src/test/miner_tests.cpp
@@ -637,7 +637,7 @@ BOOST_AUTO_TEST_CASE(CreateNewBlock_validity)
 
             // Normal usage — hardcoded nonces from BLOCKINFO:
             pblock->nNonce = bi.nonce;
-
+            block.nNonce = 0;
             // code for regenerate BLOCKINFO
             while (!CheckProofOfWork(pblock->GetYespowerPoWHash(), pblock->nBits, Assert(m_node.chainman)->GetParams().GetConsensus()) ||
                    !CheckProofOfWork(pblock->GetArgon2idPoWHash(), pblock->nBits, Assert(m_node.chainman)->GetParams().GetConsensus())) {
